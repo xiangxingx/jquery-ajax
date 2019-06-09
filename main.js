296d022570321dc71fc727400cec4ec961f8d361
xiangxingx <xiangxingx@gmail.com>
2019-06-09 17:38:15 +0800

    整理代码

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
 window.jQuery = function () {
 
 }
-window.jQuery.ajax = function ({ url, method, body, successFn, failFn, headers }) { // 给参数命名
-
-  let request = new XMLHttpRequest()
-  request.open(method, url)
-
-  for (let key in headers) {
-    let value = headers[key]
-    request.setRequestHeader(key, value)
-  }
-
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
-      console.log('响应完成')
-      if (request.status >= 200 && request.status < 300) {
-        console.log('请求成功')
-        successFn.call(undefined, request.responseText)
-      } else if (request.status >= 400) {
-        console.log('请求失败')
-        failFn.call(undefined, request)
+window.jQuery.ajax = function ({ url, method, body, headers }) { // 给参数命名
+  return new Promise(function (resolve, reject) { // 每个单词都需记住，成功调resolve，失败调reject
+    let request = new XMLHttpRequest()
+    request.open(method, url)
+    for (let key in headers) {
+      let value = headers[key]
+      request.setRequestHeader(key, value)
+    }
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) {
+        console.log('响应完成')
+        if (request.status >= 200 && request.status < 300) {
+          console.log('请求成功')
+          resolve.call(undefined, request.responseText)
+        } else if (request.status >= 400) {
+          console.log('请求失败')
+          reject.call(undefined, request)
+        }
       }
     }
-  }
-
-  request.send(body)
+    request.send(body)
+  })
 }
 
 window.$ = window.jQuery
@@ -35,9 +33,10 @@ button.addEventListener('click', () => {
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
       'xx': '18'
-    },
-    successFn: (x) => { console.log(x) },
-    failFn: (x) => { console.log(x) }
-  })
+    }
+  }).then(
+    (text) => { console.log(text) },
+    (request) => { console.log(request) }
+  )
 })
 
